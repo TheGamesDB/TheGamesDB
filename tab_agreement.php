0508a3bb4da7a47ed747175659af7da75cd495b2
mattmcla <mclaughlin.matt@gmail.com>
2010-11-25 20:58:35 +0000

    Fixed Agreement

--- tab_agreement.php
+++ tab_agreement.php
@@ -1,68 +1,60 @@
 <div class="section">
-<h1>TheTVDB.com Banner Upload Terms Agreement</h1><br>
-<span style="font-weight: bold; align: center; font-size: 12px;">Due to inconsistencies in the creation of new banners with all the new users this site has received we would like all users to review and agree to these terms before any additional banners are created.</span>
-<br /><br />
-<span style="font-weight: bold; margin-left: 15px;">All Image Guidelines</span>
-<ul><li>The Banner language should be set on upload to the language on the banner
-<li>The Banner must be jpeg not bitmap or gif.
-<li>Each Creator is limited to 3 banners per series and 3 banners per season (please try and make each banner unique and not simply the same images with different fonts
-<li>While some tv series do contain nudity please refrain from using images which contain it
-<li>Do not take another artists' banners to alter them slightly then re-upload them. Some artists have spent a good deal of time on the images they upload and it is not acceptable to alter their work without permission.
-</ul>
+    <h1>TheGamesDB Banner Upload Terms Agreement</h1><br>
+    <p><em>We would like all users to review and agree to these terms before any additional banners are created.</em></p>
+    <h2>All Image Guidelines</h2>
+    <ul>
+        <li>The Banner language should be set on upload to the language on the banner</li>
+        <li>The Banner must be jpeg not bitmap or gif.</li>
+        <li>Each Creator is limited to 3 banners per series and 3 banners per season (please try and make each banner unique and not simply the same images with different fonts.</li>
+        <li>While some tv series do contain nudity please refrain from using images which contain it.</li>
+        <li>Do not take another artists' banners to alter them slightly then re-upload them. Some artists have spent a good deal of time on the images they upload and it is not acceptable to alter their work without permission.</li>
+    </ul>
 
-<span style="font-weight: bold; margin-left: 15px;">Series &amp; Wide Season Banners</span>
-<ul><li>Banners must 758 x 140
-<br /><br />
+    <h2>Banners</h2>
+    <ul>
+        <li>Banners must be 758 x 140</li>
+        <li>Text banners generally use Arial Bold font, 27pt as the text.</li>
+    </ul>
 
-<span style="text-decoration: underline">Graphical Series Banners</span>
-<br />
-<li>Graphical Banners are defined as having a graphical/logo version of the series name
-<br /><br />
+    <h2>Blank Banners</h2>
+    <ul>
+        <li>The main requirement for blank banners is they should be blank on the left side of the banner as that is where the auto-generated text will be placed. Similar to
+    </ul>
 
-<span style="text-decoration: underline">Text Series Banners</span>
-<br />
-<li>Text Banners generally use Arial Bold font, 27pt as the text
-<br />
-<li>A Template can be downloaded <a href="http://thetvdb.com/images/banner_template.psd" target="_blank" class="postlink">from here</a>
-<br /><br />
+    <h2>Example Banners</h2>
+    <img class="banner" src="images/ayumi-banner-xblades.jpg" border="0" alt=" "/>
+    <img class="banner" src="images/Banner - Mario Joy.jpg" border="0" alt=" "/>
+    <img class="banner" src="images/Super-Mario-Land-GB-banner.jpg" border="0" alt=" "/>
 
-<span style="text-decoration: underline">Blank Banners</span>
-<br />
-<li>The main requirement for blank banners is they should be blank on the left side of the banner as that is where the auto-generated text will be placed. Similar to
-</ul>
-<img src="http://thetvdb.com/banners/blank/73545.jpg" border="0" alt=" "/><br /><br />
-<span style="font-weight: bold; margin-left: 15px;">Season/DVD Cover style Banners</span>
-<ul><li>Banners must be 400 x 578
-<br />
-<li>We very much understand that for some series decent images are very  difficult to come by and so repetition of images may be necessary. But if this is necessary try to make each season a bit different like these so that it is a bit more obvious what season you are looking at.
-<br />
-<img src="http://thetvdb.com/banners/_cache/seasons/489-1.jpg" border="0"  alt=" "/> <img src="http://thetvdb.com/banners/_cache/seasons/489-2.jpg" border="0" alt=" "/>
-</ul>
-<br />
-<span style="font-weight: bold; margin-left: 15px;">Resources</span>
-<ul><span style="text-decoration: underline">Image Sources</span>
-<br />
-<li><a href="http://www.amazon.com" target="_blank" class="postlink">http://www.amazon.com</a>
-<li><a href="http://www.cdcovers.cc/covers.php" target="_blank" class="postlink">http://www.cdcovers.cc/covers.php</a>
-<li><a href="http://www.google.com/images" target="_blank" class="postlink">http://www.google.com/images</a>
-<li><a href="http://amazon.co.uk" target="_blank" class="postlink">http://amazon.co.uk</a>
-<li><a href="http://www.amazon.ca" target="_blank" class="postlink">http://www.amazon.ca</a>
-<li><a href="http://www.covertarget.com" target="_blank" class="postlink">http://www.covertarget.com</a>
-<li><a href="http://tvshowsondvd.com" target="_blank" class="postlink">http://tvshowsondvd.com</a>
-<li><a href="http://www.dvdplanet.com" target="_blank" class="postlink">http://www.dvdplanet.com</a>
-<li><a href="http://www.tvonmedia.com/" target="_blank" class="postlink">http://www.tvonmedia.com/</a>
-<br /><br />
+    <h2>Resources</h2>
 
-<span style="text-decoration: underline">Free / commercial movie / tv-show fonts:</span>
-<br />
-<li><a href="http://www.typenow.net/themed.htm" target="_blank" class="postlink">http://www.typenow.net/themed.htm</a>
-</ul>
-<form action="<?=$fullurl?>" method="POST" name="agreeform">
-	  <input name="agreecheck" type="checkbox" onClick="agreesubmit(this)"&gt;<a href="/?tab=agreement">I have read and accept the above terms and conditions</a><br>
-	  <input type="submit" name="function" value="Terms Agreement" disabled class="submit_long">
-<script>
-//change two names below to your form's names
-document.forms.agreeform.agreecheck.checked=false
-</script>
+    <h3>Image Sources</h3>
+    <ul>
+        <li><a href="http://www.amazon.com" target="_blank" class="postlink">amazon.com</a>
+        <li><a href="http://wikipedia.com" target="_blank" class="postlink">wikipedia.com</a>
+        <li><a href="http://imageevent.com/gamescans" target="_blank" class="postlink">imageevent.com/gamescans</a>
+        <li><a href="http://giantbomb.com" target="_blank" class="postlink">giantbomb.com</a>
+        <li><a href="http://gamesradar.com/" target="_blank" class="postlink">gamesradar.com/</a>
+        <li><a href="http://gamepro.com/pc/" target="_blank" class="postlink">gamepro.com/pc/</a>
+        <li><a href="http://gamespy.com" target="_blank" class="postlink">gamespy.com</a>
+        <li><a href="http://game-walls.com/" target="_blank" class="postlink">game-walls.com/</a>
+        <li><a href="http://gamewallpapers.com/" target="_blank" class="postlink">gamewallpapers.com/</a> (high resolution)
+        <li><a href="http://vgboxart.com/" target="_blank" class="postlink">vgboxart.com/</a>
+        <li><a href="http://gameboxart.com/gba.html" target="_blank" class="postlink">gameboxart.com/gba.html</a>
+        <li><a href="http://emumovies.com/forums/index.php?/gallery/category/2-artwork-packs/" target="_blank" class="postlink">emumovies.com/</a>
+        <li><a href="http://emumovies.com/forums/index.php?app=ccs" target="_blank" class="postlink">emumovies.com/</a>
+    </ul>
 
+    <h3>Fonts:</h3>
+    <ul>
+        <li><a href="http://www.dafont.com/" target="_blank" class="postlink">http://www.dafont.com/</a>
+    </ul>
+    <form action="<?= $fullurl ?>" method="POST" name="agreeform">
+        <input name="agreecheck" type="checkbox" onClick="agreesubmit(this)"&gt;<a href="/?tab=agreement">I have read and accept the above terms and conditions</a><br>
+        <input type="submit" name="function" value="Terms Agreement" disabled class="submit_long">
+        <script>
+            //change two names below to your form's names
+            document.forms.agreeform.agreecheck.checked=false
+        </script>
+    </form>
 </div>

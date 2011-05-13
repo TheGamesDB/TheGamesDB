d486986e81565a0552f4049702f4e97dc8b18608
smidley <smidley@gmail.com>
2011-05-13 23:57:48 +0000

    Updated wording to be more accurate.

--- tab_agreement.php
+++ tab_agreement.php
@@ -1,6 +1,33 @@
 <div class="section">
-    <h1>TheGamesDB Banner Upload Terms Agreement</h1><br>
-    <p><em>We would like all users to review and agree to these terms before any additional banners are created.</em></p>
+    <h1>TheGamesDB Terms and Conditions</h1><br>
+    <p><em>We would like all users to review and agree to these terms before using the site.</em></p>
+	
+	<h2>General Guidelines</h2>
+	<p>Please take a minute to read these guidelines. We are very excited about people getting together and helping to make something great, but we also need to work together to keep some sort of order around here!
+	<br>
+	<br>
+	<u>TheGamesDb.net WIKI:</u><br>
+	We ask that anyone who plans on becoming a part of TheGamesDB.net community please takes the time to read over the WIKI as it will make your experience more worthwhile. We know the WIKI isn’t perfect. If you see something that needs to be added, edited or deleted, please post in the forums.
+	<br>
+	<br>
+	<u>Search the Site:</u><br>
+	Always do a search for a game before posting a new one.  Chances are, the game you want to post has already been submitted.  The titles may be different than what you were expecting, and if you have one that you feel is more correct for a currently posted game, please let us know in the forums and we will get it changed.  Never submit a game title that has any modifications (like the console name included).  A good practice is to see what the game is titled on Wikipedia.
+	<br>
+	<br>
+	<u>Sources of information:</u><br>
+	Your first stop for getting information should ALWAYS be an official site for the game. Sometimes, the official sites don't list complete information, so from there you'll want to hit sites that are either edited by professionals or checked for accuracy on a regular basis. These sites include wikipedia.org, gamespy.com, ign.com, and gamespot.com.
+For the titles of games, I generally follow the formatting used on Wikipedia since it's widely agreed upon. For standard game titles, MAKE SURE you capitalize the first letter in the title. If we have inconsistencies with capitalization, things will start to look very messy.
+<br>
+<br>
+	<u>Being a Community:</u><br>
+	We know that the site isn’t perfect. That’s why we need you! You think something’s wrong? Question it! You think something is unclear? Let us know! You have the greatest idea for the site that it would eliminate all issues for the rest of time? PLEASE TELL US! We are listening! We understand that YOU are the key to success here, and suggestions and feature requests have an extremely good chance of being implemented. Ask away!
+	<br>
+	<br>
+	<u>Uploading Images:</u><br>
+	Artwork for games can be some amazing pieces of work. We love the way artwork makes the programs that use the API look. Without the artwork, we would just have text! With that being said, we ask that you be smart about the images that you upload. We encourage you to find artwork and create your own, but please don't upload anything that is blurry, pixelated, or any sort of low quality. One bad image is all it takes to ruin the look of a game. This is also why we ask that you follow the resolution requirements we have set for image types. Also, DO NOT upload images that contain nudity. This includes animated nudity. Just because it is a cartoon/anime does not make the nudity okay. Keep in mind that people of ALL AGES browse this site, and we need to protect children from viewing material deemed inappropriate by applicable laws. If you upload an image of poor quality or inappropriate material, your upload privileges will be disallowed.
+	
+	</p>
+	
     <h2>All Image Guidelines</h2>
     <ul>
         <li>The Banner language should be set on upload to the language on the banner</li>
@@ -9,16 +36,19 @@
         <li>While some tv series do contain nudity please refrain from using images which contain it.</li>
         <li>Do not take another artists' banners to alter them slightly then re-upload them. Some artists have spent a good deal of time on the images they upload and it is not acceptable to alter their work without permission.</li>
     </ul>
+	
+	    <h2>Fanart</h2>
+    <ul>
+        <li>Fanart must be 1280x720 or 1920x1080</li>
+        <li>Images must be of good quality. We don't want blurry or pixelated images.</li>
+		<li>Look at the image you're using... Can this be used for more than one game? If you have a picture that is just Mario, you can probably use this as artwork for multiple mario games. This is perfectly acceptable. However, if artwork is title specific, please don't use it on another title in the game series that it doesn't apply to. This would be something like a picture of a character that shows up in one game like Super Mario Bros. 2, but isn't in Super Mario Bros.</li>
+		
+    </ul>
 
     <h2>Banners</h2>
     <ul>
-        <li>Banners must be 758 x 140</li>
+        <li>Banners must be 760 x 140</li>
         <li>Text banners generally use Arial Bold font, 27pt as the text.</li>
-    </ul>
-
-    <h2>Blank Banners</h2>
-    <ul>
-        <li>The main requirement for blank banners is they should be blank on the left side of the banner as that is where the auto-generated text will be placed. Similar to
     </ul>
 
     <h2>Example Banners</h2>
@@ -34,15 +64,15 @@
         <li><a href="http://wikipedia.com" target="_blank" class="postlink">wikipedia.com</a>
         <li><a href="http://imageevent.com/gamescans" target="_blank" class="postlink">imageevent.com/gamescans</a>
         <li><a href="http://giantbomb.com" target="_blank" class="postlink">giantbomb.com</a>
-        <li><a href="http://gamesradar.com/" target="_blank" class="postlink">gamesradar.com/</a>
-        <li><a href="http://gamepro.com/pc/" target="_blank" class="postlink">gamepro.com/pc/</a>
+        <li><a href="http://gamesradar.com/" target="_blank" class="postlink">gamesradar.com</a>
+        <li><a href="http://gamepro.com/pc/" target="_blank" class="postlink">gamepro.com</a>
         <li><a href="http://gamespy.com" target="_blank" class="postlink">gamespy.com</a>
-        <li><a href="http://game-walls.com/" target="_blank" class="postlink">game-walls.com/</a>
-        <li><a href="http://gamewallpapers.com/" target="_blank" class="postlink">gamewallpapers.com/</a> (high resolution)
+        <li><a href="http://game-walls.com/" target="_blank" class="postlink">game-walls.com</a>
+        <li><a href="http://gamewallpapers.com/" target="_blank" class="postlink">gamewallpapers.com</a> (high resolution)
         <li><a href="http://vgboxart.com/" target="_blank" class="postlink">vgboxart.com/</a>
         <li><a href="http://gameboxart.com/gba.html" target="_blank" class="postlink">gameboxart.com/gba.html</a>
-        <li><a href="http://emumovies.com/forums/index.php?/gallery/category/2-artwork-packs/" target="_blank" class="postlink">emumovies.com/</a>
-        <li><a href="http://emumovies.com/forums/index.php?app=ccs" target="_blank" class="postlink">emumovies.com/</a>
+        <li><a href="http://emumovies.com/forums/index.php?app=ccs" target="_blank" class="postlink">emumovies.com</a>
+		<li><a href="http://images.google.com" target="_blank" class="postlink">Google Image Search</a>
     </ul>
 
     <h3>Fonts:</h3>

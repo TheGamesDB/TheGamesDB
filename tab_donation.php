69c49e0296791f6f2081ddd3246aacbb1ef59173
smidley <smidley@gmail.com>
2011-05-17 23:21:52 +0000

    New donations page created.

--- tab_donation.php
+++ tab_donation.php
@@ -3,8 +3,8 @@
 	<!-- Left column -->
 	<td valign="top" width="70%">
 		<div class="section">
-		<h1>Donate to TheTVDB.com</h1>
-		<p>We are happy to continue offering free access to both the website and the API interfaces, but it does cost us money each month.  Our Google Ads are uninvasive, but will not properly cover the cost of our new hosting solution that will allow us to properly set up mirroring.  Please consider donating even a few dollars, pesos, or gold bullion to our project.</p>
+		<h1>Donate to TheGamesDB.net</h1>
+		<p>We are happy to continue offering free access to both the website and the API interfaces, but it does cost us money each month.  Please consider donating something so we can keep the site going!</p>
 		</div>
 	</td>
 
@@ -12,22 +12,18 @@
 	<!-- Right column -->
 	<td valign="top" width="30%">
 		<div class="section">
-		<h1>Donation Options</h1>
 		<p>Paypal donation:
 		<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
-		<input type="hidden" name="cmd" value="_s-xclick">
-		<input type="image" src="https://www.paypal.com/en_US/i/btn/x-click-but21.gif" border="0" name="submit" alt="Make payments with PayPal - it's fast, free and secure!">
-		<img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
-		<input type="hidden" name="encrypted" value="-----BEGIN PKCS7-----MIIHRwYJKoZIhvcNAQcEoIIHODCCBzQCAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYBXIPvPO/B/P950q/jzwdr9QdqlZbFe2uoCaQNjU4hNTVG7ndxXi/6R+K8dWAeUm6gABkXRrGBBeRf6c2b+6ZIHhx5jKMbHZrSFH0V0wxV1JT1sZgAPH1+B9Eclu8EbpwQAfQGHAmHmQeEMO2aWxdN/7wlIKq0mfioot5/8cjyKGjELMAkGBSsOAwIaBQAwgcQGCSqGSIb3DQEHATAUBggqhkiG9w0DBwQITM7sJ1iltKyAgaCEaA+f5TsKoaGU6kj4covEulkZZi4pPpSEGaKAm64yYqS3Ln+s0pMFdw3IKk4eeR5kvMORc0+xFgj7vMdjcuROKdKh/u+2Mv25SsFoeQuRF9VsgcOpSwvYpGBeSPqd/ion1qRUbkwmuL2GxLcFsx/aY1MmGM0zhspQJ2kgDXpdQPZJdlESakttZBFp+IHhBMCqjRmVVCybU9fMZLSjX0O8oIIDhzCCA4MwggLsoAMCAQICAQAwDQYJKoZIhvcNAQEFBQAwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tMB4XDTA0MDIxMzEwMTMxNVoXDTM1MDIxMzEwMTMxNVowgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDBR07d/ETMS1ycjtkpkvjXZe9k+6CieLuLsPumsJ7QC1odNz3sJiCbs2wC0nLE0uLGaEtXynIgRqIddYCHx88pb5HTXv4SZeuv0Rqq4+axW9PLAAATU8w04qqjaSXgbGLP3NmohqM6bV9kZZwZLR/klDaQGo1u9uDb9lr4Yn+rBQIDAQABo4HuMIHrMB0GA1UdDgQWBBSWn3y7xm8XvVk/UtcKG+wQ1mSUazCBuwYDVR0jBIGzMIGwgBSWn3y7xm8XvVk/UtcKG+wQ1mSUa6GBlKSBkTCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb22CAQAwDAYDVR0TBAUwAwEB/zANBgkqhkiG9w0BAQUFAAOBgQCBXzpWmoBa5e9fo6ujionW1hUhPkOBakTr3YCDjbYfvJEiv/2P+IobhOGJr85+XHhN0v4gUkEDI8r2/rNk1m0GA8HKddvTjyGw/XqXa+LSTlDYkqI8OwR8GEYj4efEtcRpRYBxV8KxAW93YDWzFGvruKnnLbDAF6VR5w/cCMn5hzGCAZowggGWAgEBMIGUMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbQIBADAJBgUrDgMCGgUAoF0wGAYJKoZIhvcNAQkDMQsGCSqGSIb3DQEHATAcBgkqhkiG9w0BCQUxDxcNMDcwODAyMjAxODAwWjAjBgkqhkiG9w0BCQQxFgQUjuezbdfacIhiypJnljpbPN2IPGwwDQYJKoZIhvcNAQEBBQAEgYCGEaKRkDvy89Kq4r9x53zBvgCg9b6EWoqXiCRMwfWnlUoOGz2UxGRAJTWQEs+1/CVt7SbFEjVtks+LfJ7PXLLl4BC2w3k0dsnBbi1DYbW4d82LAN5zIXBpUfqM/mVXP1SQSnJm1PI4OAUuhYwCWxLwVbo+i8i9UorNHVvfZxWJbg==-----END PKCS7-----
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="encrypted" value="-----BEGIN PKCS7-----MIIHNwYJKoZIhvcNAQcEoIIHKDCCByQCAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYBaGAlbxRop7l4u/koAWZks5KJKyX1zjuI9/LatYDhgwCVfFZViXZYaHeR6y0ma2ax3hj6WTs5EmzZ39jTHcUblsf/OlGnuYhZXhqxsYOjdZdYtKi5suYS6r28WsunYWrs7UXtv4OjvK27zbXRvZ9rzi9J6ZCFiIRzvEiYSvj7kFzELMAkGBSsOAwIaBQAwgbQGCSqGSIb3DQEHATAUBggqhkiG9w0DBwQIsCsJ7p4V7/2AgZA3jPnpXillclAYGslSpj511aDU838UU5d9l4hW4PbJJ18GhFLepFA4ekgGaQBnM8LMvJt9LiVqqR2Kr1Hxqtm46r7qrhuz4/6x89s/19y5yVU9VHtUjpk73HTP9JXexZuuxZFp5h3k0g2TWgGItH4MQk8iLro6aU4mjx8+sUpmTNKjg7xyMSFLbbi7n+iwz1mgggOHMIIDgzCCAuygAwIBAgIBADANBgkqhkiG9w0BAQUFADCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20wHhcNMDQwMjEzMTAxMzE1WhcNMzUwMjEzMTAxMzE1WjCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20wgZ8wDQYJKoZIhvcNAQEBBQADgY0AMIGJAoGBAMFHTt38RMxLXJyO2SmS+Ndl72T7oKJ4u4uw+6awntALWh03PewmIJuzbALScsTS4sZoS1fKciBGoh11gIfHzylvkdNe/hJl66/RGqrj5rFb08sAABNTzDTiqqNpJeBsYs/c2aiGozptX2RlnBktH+SUNpAajW724Nv2Wvhif6sFAgMBAAGjge4wgeswHQYDVR0OBBYEFJaffLvGbxe9WT9S1wob7BDWZJRrMIG7BgNVHSMEgbMwgbCAFJaffLvGbxe9WT9S1wob7BDWZJRroYGUpIGRMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbYIBADAMBgNVHRMEBTADAQH/MA0GCSqGSIb3DQEBBQUAA4GBAIFfOlaagFrl71+jq6OKidbWFSE+Q4FqROvdgIONth+8kSK//Y/4ihuE4Ymvzn5ceE3S/iBSQQMjyvb+s2TWbQYDwcp129OPIbD9epdr4tJOUNiSojw7BHwYRiPh58S1xGlFgHFXwrEBb3dgNbMUa+u4qectsMAXpVHnD9wIyfmHMYIBmjCCAZYCAQEwgZQwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tAgEAMAkGBSsOAwIaBQCgXTAYBgkqhkiG9w0BCQMxCwYJKoZIhvcNAQcBMBwGCSqGSIb3DQEJBTEPFw0xMTA1MTcyMzA4NDFaMCMGCSqGSIb3DQEJBDEWBBRovZK+4oiumSx4hOObpMdKIkdgBDANBgkqhkiG9w0BAQEFAASBgJqWlXgaVVnn2Du1bmIvHopsWp+Kp8s3pxipEF6kx7QDo6B3wjpKs3HMhwubY0+jPTdnEqmP3zfpWYPcIajMfjNLVtoKayUyX0oLoYt9/SCnBGEcdPPit0bG6/J6NtjonMSpIbU9XmJCZ0WaEaAQxpM7afe/KkazaMgis0JEQsEO-----END PKCS7-----
 ">
-		</form></p>
+<input type="image" src="https://www.paypalobjects.com/WEBSCR-640-20110429-1/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+<img alt="" border="0" src="https://www.paypalobjects.com/WEBSCR-640-20110429-1/en_US/i/scr/pixel.gif" width="1" height="1">
+</form>
+</p>
 		</div>
-
-		<div class="titlesection" style="display:none">
-		<h1>Current Donation Pool</h1>
-		<p style="font-weight: bold">The current donation pool is $149 ($232 donations - $83 hosting fee).</p>
-		<p>Thanks to all who have donated.  As we pay for our hosting and receive additional donations, this number will be modified accordingly.</p>
-		</div>
+		
+		
 	</td>
 </tr>
 </table>

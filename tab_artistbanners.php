9afc8034983e686b67844b7350c812a70039cbfe
smidley <smidley@gmail.com>
2011-05-18 07:27:49 +0000

    Changed wording to match art types.  Added recent boxart section.

--- tab_artistbanners.php
+++ tab_artistbanners.php
@@ -8,8 +8,8 @@
 
 <div class="section">
 	<h1><?=$db->username?> Banners</h1>
-  <a href="/index.php?tab=recentbanners&bannertype=series&banneruser=<?=$id?>">50 Most Recent Series Banners</a> - <a href="/index.php?tab=recentbanners&bannertype=series&banneruser=<?=$id?>&ratedonly=WhatSoundDoWombatsMake">Rated Only</a><br>
+  <a href="/index.php?tab=recentbanners&bannertype=series&banneruser=<?=$id?>">50 Most Recent Game Banners</a> - <a href="/index.php?tab=recentbanners&bannertype=series&banneruser=<?=$id?>&ratedonly=WhatSoundDoWombatsMake">Rated Only</a><br>
 	<a href="/index.php?tab=recentbanners&bannertype=fanart&banneruser=<?=$id?>">20 Most Recent Fan Art</a> - <a href="/index.php?tab=recentbanners&bannertype=fanart&banneruser=<?=$id?>&ratedonly=WhatSoundDoWombatsMake">Rated Only</a> - <a href="/index.php?tab=recentbanners&bannertype=fanart&banneruser=<?=$id?>&artistcolorsmissing=Moo">Missing Artist Colors</a><br>
-	<a href="/index.php?tab=recentbanners&bannertype=season&banneruser=<?=$id?>">20 Most Recent Season Banners</a> - <a href="/index.php?tab=recentbanners&bannertype=season&banneruser=<?=$id?>&ratedonly=WhatSoundDoWombatsMake">Rated Only</a><br>
-	<a href="/index.php?tab=recentbanners&bannertype=seasonwide&banneruser=<?=$id?>">50 Most Recent Wide Season Banners</a> - <a href="/index.php?tab=recentbanners&bannertype=seasonwide&banneruser=<?=$id?>&ratedonly=WhatSoundDoWombatsMake">Rated Only</a><br>
+	<a href="/index.php?tab=recentbanners&bannertype=boxart&banneruser=<?=$id?>">20 Most Recent Boxart</a> - <a href="/index.php?tab=recentbanners&bannertype=boxart&banneruser=<?=$id?>&ratedonly=WhatSoundDoWombatsMake">Rated Only</a><br>
+	<br>
 </div>

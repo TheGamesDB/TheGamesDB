b1171f207bb4d13b8b9b363293d0fb2149744441
smidley <smidley@gmail.com>
2011-05-18 20:04:30 +0000

    Changed more wording and removed season banners wide.

--- tab_artistbanners.php
+++ tab_artistbanners.php
@@ -7,9 +7,8 @@
 ?>
 
 <div class="section">
-	<h1><?=$db->username?> Banners</h1>
+	<h1><?=$db->username?> Art Contributions</h1>
   <a href="/index.php?tab=recentbanners&bannertype=series&banneruser=<?=$id?>">50 Most Recent Game Banners</a> - <a href="/index.php?tab=recentbanners&bannertype=series&banneruser=<?=$id?>&ratedonly=WhatSoundDoWombatsMake">Rated Only</a><br>
 	<a href="/index.php?tab=recentbanners&bannertype=fanart&banneruser=<?=$id?>">20 Most Recent Fan Art</a> - <a href="/index.php?tab=recentbanners&bannertype=fanart&banneruser=<?=$id?>&ratedonly=WhatSoundDoWombatsMake">Rated Only</a> - <a href="/index.php?tab=recentbanners&bannertype=fanart&banneruser=<?=$id?>&artistcolorsmissing=Moo">Missing Artist Colors</a><br>
 	<a href="/index.php?tab=recentbanners&bannertype=boxart&banneruser=<?=$id?>">20 Most Recent Boxart</a> - <a href="/index.php?tab=recentbanners&bannertype=boxart&banneruser=<?=$id?>&ratedonly=WhatSoundDoWombatsMake">Rated Only</a><br>
-	<br>
 </div>

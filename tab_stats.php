1791c7bb8243db259d1ec94de619a53e46e78fd1
smidley <smidley@gmail.com>
2011-05-18 22:39:15 +0000

    Added stats page.

---
+++ tab_stats.php
@@ -0,0 +1,8 @@
+<div class="section">
+	<center><h1>Site Reports and Statistics</h1>
+	<ul><a href=http://thegamesdb.net/?tab=bannerartists>Top 50 Art Contributors</a></ul>
+	<ul><a href=http://thegamesdb.net/?tab=recentbanners&bannertype=series>50 Most Recent Game Banners</a></ul>
+	<ul><a href=http://thegamesdb.net/?tab=recentbanners&bannertype=fanart>20 Most Recent Fanart Images</a></ul>
+	<ul><a href=http://thegamesdb.net/?tab=recentbanners&bannertype=boxart>50 Most Recent Boxart Images</a></ul>
+	</center>
+</div>

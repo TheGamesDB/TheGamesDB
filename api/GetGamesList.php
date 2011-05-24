fe6bae5adee8537cca02f50a2387b5a9489ba100
smidley <smidley@gmail.com>
2011-05-24 03:42:57 +0000

    Have added a new functionality to the api to allow returning of a loose list of search matches.

---
+++ api/GetGamesList.php
@@ -0,0 +1,59 @@
+<?php
+## Interface that allows clients to get
+## seriesid using name
+## Parameters:
+##   $_REQUEST["name"]
+##   $_REQUEST["language"]		(optional)
+##   $_REQUEST["user"]			(optional... overrides language setting)
+##
+## Returns:
+##   XML items holding the games that matches the name string
+
+## Include functions, db connection, etc
+include("include.php");
+
+## Prepare the search string
+$name = addslashes(stripslashes(stripslashes($_REQUEST["name"])));
+$name = str_replace(array(' - ', '-'), '%', $name);
+
+//$language		= $_REQUEST["language"];
+$user = $_REQUEST["user"];
+
+if (empty($name)) {
+    print "<Error>A name is required</Error>\n";
+    exit;
+} else {
+    if (isset($name)) {
+        if (strpos($name, ", The")) {
+            $name = "The " . substr($name, 0, strpos($name, ", The"));
+        }
+    }
+    print "<Data>\n";
+}
+
+$query;
+if (isset($name) && !empty($name)) {
+    $query = "SELECT id FROM games WHERE GameTitle LIKE '%$name%' ORDER BY GameTitle ASC";
+}
+$result = mysql_query($query) or die('Query failed: ' . mysql_error());
+
+while ($obj = mysql_fetch_object($result)) {
+    print "<Game>\n";
+
+    // Base Info
+    $subquery = "SELECT id, GameTitle, ReleaseDate FROM games WHERE id={$obj->id}";
+    $baseResult = mysql_query($subquery) or die('Query failed: ' . mysql_error());
+    $baseObj = mysql_fetch_object($baseResult);
+    foreach ($baseObj as $key => $value) {
+        ## Prepare the string for output
+        if (!empty($value)) {
+            $value = xmlformat($value, $key);
+            print "<$key>$value</$key>\n";
+        }
+    }
+
+    ## End XML item
+    print "</Game>\n";
+}
+?>
+</Data>

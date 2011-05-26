3ae20b2bb3b3aa5b92139679addbc79afd2d0af7
smidley <smidley@gmail.com>
2011-05-26 17:36:38 +0000

    New Showcase Page.

---
+++ tab_showcase.php
@@ -0,0 +1,95 @@
+<h1 class="arcade">Showcase</h1>
+
+<p>TheGamesDB.net is proud to provide services for the following featured projects that utilise our data to bring the world of gaming to life.</p>
+
+<!-- START GameBrowser Feature -->
+<div style="width: 800px; background-color: #000; color: #FFF; margin: auto; padding: 5px 30px 15px 30px; border: 2px solid #666; outline: 0px solid #666; text-align: justify; background-image: url(images/showcase/bg-featured-panel.jpg); background-repeat: no-repeat; background-position: bottom center; border-radius: 25px; outline-radius: 25px; margin-bottom: 20px;">
+	<img src="images/showcase/feature-gamebrowser.jpg" style="float: left; padding-right: 30px; padding-bottom: 20px;" />
+	<h1 class="arcade"><span style="color: #FFF;">Game</span><span style="color: #00CC3F;">Browser</span></h1>
+	<p><br />GameBrowser is a add-on for the popular MediaBrowser HTPC software.</p>
+	<p>It enables users to easily browse through their collection of games utilising an attractive and intuitive interface, providing a blend between the gaming and home-theater experiences.</p>
+	<p>GameBrowser uses our API as a source for game artwork &amp; metadata, enabling users to have a media-rich and informative way of browsing their collection.</p>
+	
+	<div style="clear: both;"></div>
+	
+	<div style="float: left; width: 315px;">
+		<h2 class="arcade" style="color: #FF004F;">Info:</h2>
+		<p><strong>Type:</strong> MediaBrowser Add-On</p>
+		<p><strong>GameBrowser:</strong> <a href="http://www.gamebrowser.tv" target="_blank" style="color: #FFFFFF;">www.gamebrowser.tv</a></p>
+		<p><strong>MediaBrowser:</strong> <a href="http://www.mediabrowser.tv" target="_blank" style="color: #FFFFFF;">www.mediabrowser.tv</a></p>
+	</div>
+	
+	<div style="float: left;">
+		<h2 class="arcade" style="color: #FF4F00;">Media:</h2>
+		<a rel="shadowbox[gamebrowser];width=560;height=349" href="http://www.youtube.com/embed/BXDDraeFROg"><img src="images/showcase/thumb-gamebrowser-video1.jpg"  style="float:left;" /></a>
+		<a rel="shadowbox[gamebrowser]" href="images/showcase/gamebrowser-image1.png"><img src="images/showcase/gamebrowser-image1.png"  style="float:left; width: 100px; height: 100px; padding-left: 10px;" /></a>
+		<a rel="shadowbox[gamebrowser]" href="images/showcase/gamebrowser-image2.png"><img src="images/showcase/gamebrowser-image2.png"  style="float:left; width: 100px; height: 100px; padding-left: 10px;" /></a>
+		<a rel="shadowbox[gamebrowser]" href="images/showcase/gamebrowser-image3.png"><img src="images/showcase/gamebrowser-image3.png"  style="float:left; width: 100px; height: 100px; padding-left: 10px;" /></a>
+		<div style="clear: both;"></div>
+	</div>
+	
+	<div style="clear: both;"></div>
+	
+</div>
+<!-- END GameBrowser Feature -->
+
+<!-- START XBMC-Launcher Feature -->
+<div style="width: 800px; background-color: #000; color: #FFF; margin: auto; padding: 5px 30px 15px 30px; border: 2px solid #666; outline: 0px solid #666; text-align: justify; background-image: url(images/showcase/bg-featured-panel.jpg); background-repeat: no-repeat; background-position: bottom center; border-radius: 25px; outline-radius: 25px; margin-bottom: 20px;">
+	<img src="images/showcase/feature-xbmclauncher.jpg" style="float: left; padding-right: 30px; padding-bottom: 20px;" />
+	<h1 class="arcade"><span style="color: #00CC3F;">XBMC</span><span style="color: #FFF;">Launcher</span></h1>
+	<p><br />XBMC-Launcher is a plugin that used to launch and run windows or linux applications including emulators within the XBMC GUI.</p>
+	<p>It will allow you to browse your games &amp; apps collection with your remote displaying images scraped from our database wherever it can.</p>
+	<p>It is available to download &amp; and auto-install through the XBMC Application Repository.</p>
+	
+	<div style="clear: both;"></div>
+	
+	<div style="float: left; width: 315px;">
+		<h2 class="arcade" style="color: #FF004F;">Info:</h2>
+		<p><strong>Type:</strong> XBMC Add-On</p>
+		<p><strong>XBMC:</strong> <a href="http://xbmc.org/" target="_blank" style="color: #FFFFFF;">http://xbmc.org/</a></p>
+		<p><strong>Project:</strong> <a href="http://code.google.com/p/xbmc-launcher/" target="_blank" style="color: #FFFFFF;">code.google.com/p/xbmc-launcher/</a></p>
+	</div>
+	
+	<div style="float: left;">
+		<h2 class="arcade" style="color: #FF4F00;">Media:</h2>
+		<a rel="shadowbox[xbmclauncher]" href="images/showcase/xbmclauncher-image1.png"><img src="images/showcase/xbmclauncher-image1.png"  style="float:left; width: 100px; height: 100px; padding-left: 20px;" /></a>
+		<a rel="shadowbox[xbmclauncher]" href="images/showcase/xbmclauncher-image2.png"><img src="images/showcase/xbmclauncher-image2.png"  style="float:left; width: 100px; height: 100px; padding-left: 20px;" /></a>
+		<a rel="shadowbox[xbmclauncher]" href="images/showcase/xbmclauncher-image3.png"><img src="images/showcase/xbmclauncher-image3.png"  style="float:left; width: 100px; height: 100px; padding-left: 20px;" /></a>
+		<a rel="shadowbox[xbmclauncher]" href="images/showcase/xbmclauncher-image4.png"><img src="images/showcase/xbmclauncher-image4.png"  style="float:left; width: 100px; height: 100px; padding-left: 20px;" /></a>
+		<div style="clear: both;"></div>
+	</div>
+	
+	<div style="clear: both;"></div>
+	
+</div>
+<!-- END XBMC-Launcher Feature -->
+
+<!-- START GameBrowser Feature -->
+<div style="width: 800px; background-color: #000; color: #FFF; margin: auto; padding: 5px 30px 15px 30px; border: 2px solid #666; outline: 0px solid #666; text-align: justify; background-image: url(images/showcase/bg-featured-panel.jpg); background-repeat: no-repeat; background-position: bottom center; border-radius: 25px; outline-radius: 25px; margin-bottom: 20px;">
+	<img src="images/showcase/feature-romcollectionbrowser.jpg" style="float: left; padding-right: 30px; padding-bottom: 20px;" />
+	<h1 class="arcade"><span style="color: #FFF;">Rom</span><span style="color: #00CC3F;">Collection</span><span style="color: #fff;">Browser</span></h1>
+	<p>Rom Collection Browser is an XBMC script for browsing and launching emulator roms.</p>
+	<p>Some of it's features include: scraping artwork and game information from various sources <em>(including thegamesdb.net)</em>, browsing games by console, genre, year and publisher, multiple views of the main window, dedicated MAME views to display cabinets &amp; marquees and launch roms &amp; standalone games from within XBMC.</p>
+	
+	<div style="clear: both;"></div>
+	
+	<div style="float: left; width: 315px;">
+		<h2 class="arcade" style="color: #FF004F;">Info:</h2>
+		<p><strong>Type:</strong> XBMC Add-On</p>
+		<p><strong>XBMC:</strong> <a href="http://xbmc.org/" target="_blank" style="color: #FFFFFF;">http://xbmc.org/</a></p>
+		<p><strong>Project:</strong> <a href="http://code.google.com/p/romcollectionbrowser/" target="_blank" style="color: #FFFFFF;">code.google.com/p/romcollectionbrowser/</a></p>
+	</div>
+	
+	<div style="float: left;">
+		<h2 class="arcade" style="color: #FF4F00;">Media:</h2>
+		<a rel="shadowbox[romcollectionbrowser];width=640;height=510" href="http://www.youtube.com/embed/_xvGM8eHWB0"><img src="images/showcase/thumb-romcollectionbrowser-video1.jpg"  style="float:left;" /></a>
+		<a rel="shadowbox[romcollectionbrowser]" href="images/showcase/romcollectionbrowser-image1.jpg"><img src="images/showcase/romcollectionbrowser-image1.jpg"  style="float:left; width: 100px; height: 100px; padding-left: 10px;" /></a>
+		<a rel="shadowbox[romcollectionbrowser]" href="images/showcase/romcollectionbrowser-image2.jpg"><img src="images/showcase/romcollectionbrowser-image2.jpg"  style="float:left; width: 100px; height: 100px; padding-left: 10px;" /></a>
+		<a rel="shadowbox[romcollectionbrowser]" href="images/showcase/romcollectionbrowser-image3.jpg"><img src="images/showcase/romcollectionbrowser-image3.jpg"  style="float:left; width: 100px; height: 100px; padding-left: 10px;" /></a>
+		<div style="clear: both;"></div>
+	</div>
+	
+	<div style="clear: both;"></div>
+	
+</div>
+<!-- END GameBrowser Feature -->

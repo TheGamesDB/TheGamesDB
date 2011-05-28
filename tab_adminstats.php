4f2cc0b9c181c66dc489525045d3320eb7529e2a
smidley <smidley@gmail.com>
2011-05-28 20:01:15 +0000

    Merged changes from flexage branch.

--- tab_adminstats.php
+++ tab_adminstats.php
@@ -20,16 +20,18 @@ if ($adminuserlevel == 'ADMINISTRATOR') {
 					<tr>
 						<th style="background-color: #333; color: #FFF;">Game ID</th>
 						<th style="background-color: #333; color: #FFF;">Game Title</th>
+						<th style="background-color: #333; color: #FFF;">Platform</th>
 						<th style="background-color: #333; color: #FFF;">Locked By</th>
 					</tr>
 				<?php
 				$lockedcount = 0;
-				$result = mysql_query(" SELECT games.id, games.GameTitle, games.lockedby, users.username FROM games, users WHERE locked = 'yes' AND games.lockedby = users.id ORDER BY games.GameTitle ASC");
+				$result = mysql_query(" SELECT games.id, games.GameTitle, games.Platform, games.lockedby, users.username FROM games, users WHERE locked = 'yes' AND games.lockedby = users.id ORDER BY games.GameTitle ASC");
 				while($row = mysql_fetch_assoc($result)) {
 					?>
 					<tr>
 						<td><?php echo $row[id]; ?></td>
 						<td align="left"><a href="?tab=game&id=<?php echo $row[id]; ?>&lid=1"><?php echo $row[GameTitle]; ?></a></td>
+						<td><?php echo $row[Platform]; ?></td>
 						<td><?php echo $row[username]; ?></td>
 					</tr>
 					<?php
@@ -37,7 +39,7 @@ if ($adminuserlevel == 'ADMINISTRATOR') {
 				}
 				?>
 					<tr>
-						<td colspan="3" style="background-color: #EEE; font-weight: bold;">Total Locked Games: <?php echo $lockedcount; ?></td>
+						<td colspan="4" style="background-color: #EEE; font-weight: bold;">Total Locked Games: <?php echo $lockedcount; ?></td>
 					</tr>
 				</table>
 				<?php
@@ -50,22 +52,24 @@ if ($adminuserlevel == 'ADMINISTRATOR') {
 					<tr>
 						<th style="background-color: #333; color: #FFF;">Game ID</th>
 						<th style="background-color: #333; color: #FFF;">Game Title</th>
+						<th style="background-color: #333; color: #FFF;">Platform</th>
 					</tr>
 				<?php
 				$missingcount = 0;
-				$result = mysql_query(" SELECT games.id, games.GameTitle FROM games WHERE games.Overview IS NULL ORDER BY games.GameTitle ASC ");
+				$result = mysql_query(" SELECT games.id, games.GameTitle, games.Platform FROM games WHERE games.Overview IS NULL ORDER BY games.GameTitle ASC ");
 				while($row = mysql_fetch_assoc($result)) {
 					?>
 					<tr>
 						<td><?php echo $row[id]; ?></td>
 						<td align="left"><a href="?tab=game&id=<?php echo $row[id]; ?>&lid=1"><?php echo $row[GameTitle]; ?></a></td>
+						<td><?php echo $row[Platform]; ?></td>
 					</tr>
 					<?php
 					$missingcount++;
 				}
 				?>
 					<tr>
-						<td colspan="2" style="background-color: #EEE; font-weight: bold;" >Total Games Missing Overview: <?php echo $missingcount; ?></td>
+						<td colspan="3" style="background-color: #EEE; font-weight: bold;" >Total Games Missing Overview: <?php echo $missingcount; ?></td>
 					</tr>
 				</table>
 				<?php
@@ -106,22 +110,24 @@ if ($adminuserlevel == 'ADMINISTRATOR') {
 					<tr>
 						<th style="background-color: #333; color: #FFF;">Game ID</th>
 						<th style="background-color: #333; color: #FFF;">Game Title</th>
+						<th style="background-color: #333; color: #FFF;">Platform</th>
 					</tr>
 				<?php
 				$missingcount = 0;
-				$result = mysql_query(" SELECT games.id, games.GameTitle FROM games WHERE games.Genre IS NULL ORDER BY games.GameTitle ASC ");
+				$result = mysql_query(" SELECT games.id, games.GameTitle, games.Platform FROM games WHERE games.Genre IS NULL ORDER BY games.GameTitle ASC ");
 				while($row = mysql_fetch_assoc($result)) {
 					?>
 					<tr>
 						<td><?php echo $row[id]; ?></td>
 						<td align="left"><a href="?tab=game&id=<?php echo $row[id]; ?>&lid=1"><?php echo $row[GameTitle]; ?></a></td>
+						<td><?php echo $row[Platform]; ?></td>
 					</tr>
 					<?php
 					$missingcount++;
 				}
 				?>
 					<tr>
-						<td colspan="2" style="background-color: #EEE; font-weight: bold;" >Total Games Missing Genre Data: <?php echo $missingcount; ?></td>
+						<td colspan="3" style="background-color: #EEE; font-weight: bold;" >Total Games Missing Genre Data: <?php echo $missingcount; ?></td>
 					</tr>
 				</table>
 				<?php
@@ -164,22 +170,24 @@ if ($adminuserlevel == 'ADMINISTRATOR') {
 					<tr>
 						<th style="background-color: #333; color: #FFF;">Game ID</th>
 						<th style="background-color: #333; color: #FFF;">Game Title</th>
+						<th style="background-color: #333; color: #FFF;">Platform</th>
 					</tr>
 				<?php
 				$missingcount = 0;
-				$result = mysql_query(" SELECT games.id, games.GameTitle FROM games WHERE NOT EXISTS (SELECT keyvalue FROM banners WHERE banners.keyvalue = games.id AND banners.filename LIKE '%front%') ORDER BY games.GameTitle ASC ");
+				$result = mysql_query(" SELECT games.id, games.GameTitle, games.Platform FROM games WHERE NOT EXISTS (SELECT keyvalue FROM banners WHERE banners.keyvalue = games.id AND banners.filename LIKE '%front%') ORDER BY games.GameTitle ASC ");
 				while($row = mysql_fetch_assoc($result)) {
 					?>
 					<tr>
 						<td><?php echo $row[id]; ?></td>
 						<td align="left"><a href="?tab=game&id=<?php echo $row[id]; ?>&lid=1"><?php echo $row[GameTitle]; ?></a></td>
+						<td><?php echo $row[Platform]; ?></td>
 					</tr>
 					<?php
 					$missingcount++;
 				}
 				?>
 					<tr>
-						<td colspan="2" style="background-color: #EEE; font-weight: bold;" >Total Games Missing Front Boxart: <?php echo $missingcount; ?></td>
+						<td colspan="3" style="background-color: #EEE; font-weight: bold;" >Total Games Missing Front Boxart: <?php echo $missingcount; ?></td>
 					</tr>
 				</table>
 				<?php
@@ -192,22 +200,24 @@ if ($adminuserlevel == 'ADMINISTRATOR') {
 					<tr>
 						<th style="background-color: #333; color: #FFF;">Game ID</th>
 						<th style="background-color: #333; color: #FFF;">Game Title</th>
+						<th style="background-color: #333; color: #FFF;">Platform</th>
 					</tr>
 				<?php
 				$morecount = 0;
-				$result = mysql_query(" SELECT games.id, games.GameTitle FROM games WHERE (SELECT COUNT(filename) FROM banners WHERE banners.keyvalue = games.id AND banners.filename LIKE '%front%') > 1 ORDER BY games.GameTitle ASC ");
+				$result = mysql_query(" SELECT games.id, games.GameTitle, games.Platform FROM games WHERE (SELECT COUNT(filename) FROM banners WHERE banners.keyvalue = games.id AND banners.filename LIKE '%front%') > 1 ORDER BY games.GameTitle ASC ");
 				while($row = mysql_fetch_assoc($result)) {
 					?>
 					<tr>
 						<td><?php echo $row[id]; ?></td>
 						<td align="left"><a href="?tab=game&id=<?php echo $row[id]; ?>&lid=1"><?php echo $row[GameTitle]; ?></a></td>
+						<td><?php echo $row[Platform]; ?></td>
 					</tr>
 					<?php
 					$morecount++;
 				}
 				?>
 					<tr>
-						<td colspan="2" style="background-color: #EEE; font-weight: bold;" >Total Games With 2 or More Front Boxart: <?php echo $missingcount; ?></td>
+						<td colspan="3" style="background-color: #EEE; font-weight: bold;" >Total Games With 2 or More Front Boxart: <?php echo $morecount; ?></td>
 					</tr>
 				</table>
 				<?php
@@ -220,22 +230,24 @@ if ($adminuserlevel == 'ADMINISTRATOR') {
 					<tr>
 						<th style="background-color: #333; color: #FFF;">Game ID</th>
 						<th style="background-color: #333; color: #FFF;">Game Title</th>
+						<th style="background-color: #333; color: #FFF;">Platform</th>
 					</tr>
 				<?php
 				$missingcount = 0;
-				$result = mysql_query(" SELECT games.id, games.GameTitle FROM games WHERE NOT EXISTS (SELECT keyvalue FROM banners WHERE banners.keyvalue = games.id AND banners.filename LIKE '%back%') ORDER BY games.GameTitle ASC ");
+				$result = mysql_query(" SELECT games.id, games.GameTitle, games.Platform FROM games WHERE NOT EXISTS (SELECT keyvalue FROM banners WHERE banners.keyvalue = games.id AND banners.filename LIKE '%back%') ORDER BY games.GameTitle ASC ");
 				while($row = mysql_fetch_assoc($result)) {
 					?>
 					<tr>
 						<td><?php echo $row[id]; ?></td>
 						<td align="left"><a href="?tab=game&id=<?php echo $row[id]; ?>&lid=1"><?php echo $row[GameTitle]; ?></a></td>
+						<td><?php echo $row[Platform]; ?></td>
 					</tr>
 					<?php
 					$missingcount++;
 				}
 				?>
 					<tr>
-						<td colspan="2" style="background-color: #EEE; font-weight: bold;" >Total Games Missing Back Boxart: <?php echo $missingcount; ?></td>
+						<td colspan="3" style="background-color: #EEE; font-weight: bold;" >Total Games Missing Back Boxart: <?php echo $missingcount; ?></td>
 					</tr>
 				</table>
 				<?php
@@ -248,22 +260,24 @@ if ($adminuserlevel == 'ADMINISTRATOR') {
 					<tr>
 						<th style="background-color: #333; color: #FFF;">Game ID</th>
 						<th style="background-color: #333; color: #FFF;">Game Title</th>
+						<th style="background-color: #333; color: #FFF;">Platform</th>
 					</tr>
 				<?php
 				$missingcount = 0;
-				$result = mysql_query(" SELECT games.id, games.GameTitle FROM games WHERE NOT EXISTS (SELECT keyvalue FROM banners WHERE banners.keyvalue = games.id AND banners.keytype = 'fanart') ORDER BY games.GameTitle ASC ");
+				$result = mysql_query(" SELECT games.id, games.GameTitle, games.Platform FROM games WHERE NOT EXISTS (SELECT keyvalue FROM banners WHERE banners.keyvalue = games.id AND banners.keytype = 'fanart') ORDER BY games.GameTitle ASC ");
 				while($row = mysql_fetch_assoc($result)) {
 					?>
 					<tr>
 						<td><?php echo $row[id]; ?></td>
 						<td align="left"><a href="?tab=game&id=<?php echo $row[id]; ?>&lid=1"><?php echo $row[GameTitle]; ?></a></td>
+						<td><?php echo $row[Platform]; ?></td>
 					</tr>
 					<?php
 					$missingcount++;
 				}
 				?>
 					<tr>
-						<td colspan="2" style="background-color: #EEE; font-weight: bold;" >Total Games Missing Fanart: <?php echo $missingcount; ?></td>
+						<td colspan="3" style="background-color: #EEE; font-weight: bold;" >Total Games Missing Fanart: <?php echo $missingcount; ?></td>
 					</tr>
 				</table>
 				<?php
@@ -276,22 +290,24 @@ if ($adminuserlevel == 'ADMINISTRATOR') {
 					<tr>
 						<th style="background-color: #333; color: #FFF;">Game ID</th>
 						<th style="background-color: #333; color: #FFF;">Game Title</th>
+						<th style="background-color: #333; color: #FFF;">Platform</th>
 					</tr>
 				<?php
 				$missingcount = 0;
-				$result = mysql_query(" SELECT games.id, games.GameTitle FROM games WHERE NOT EXISTS (SELECT keyvalue FROM banners WHERE banners.keyvalue = games.id AND banners.keytype = 'series') ORDER BY games.GameTitle ASC ");
+				$result = mysql_query(" SELECT games.id, games.GameTitle, games.Platform FROM games WHERE NOT EXISTS (SELECT keyvalue FROM banners WHERE banners.keyvalue = games.id AND banners.keytype = 'series') ORDER BY games.GameTitle ASC ");
 				while($row = mysql_fetch_assoc($result)) {
 					?>
 					<tr>
 						<td><?php echo $row[id]; ?></td>
 						<td align="left"><a href="?tab=game&id=<?php echo $row[id]; ?>&lid=1"><?php echo $row[GameTitle]; ?></a></td>
+						<td><?php echo $row[Platform]; ?></td>
 					</tr>
 					<?php
 					$missingcount++;
 				}
 				?>
 					<tr>
-						<td colspan="2" style="background-color: #EEE; font-weight: bold;" >Total Games Missing Banners: <?php echo $missingcount; ?></td>
+						<td colspan="3" style="background-color: #EEE; font-weight: bold;" >Total Games Missing Banners: <?php echo $missingcount; ?></td>
 					</tr>
 				</table>
 				<?php
@@ -312,6 +328,7 @@ if ($adminuserlevel == 'ADMINISTRATOR') {
 	}
 	else {
 		?>
+		<h1 class="arcade">Notice_</h1>
 		<p style="text-align: center;"><span style="color: #FF0000; font-weight: bold;">Error:</span> You must be logged in to view this page.</p>
 		<?php
 	}

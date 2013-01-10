ee457cf8fbf38e496ec0e40cdb76cd07c307b6d2
Marcel van Brakel <marcel@enrise.com>
2013-01-10 20:41:17 +0100

    Added 404 page

---
+++ tab_404.php
@@ -0,0 +1,10 @@
+<div id="gameHead">
+    <img src="/images/404.jpg" style="float:left;padding: 20px; padding-right: 50px;" />
+    <div style="text-align: left; margin-bottom: 20px; ">
+	    <h1 style="margin-top: 80px">404 - Page not found</h1>
+	    <p>The page you are looking for cannot be found.<br/>Please check your URL or notify us when this page is suppost to exist.</p>
+	    <br/>
+	    <p><a style="color: orange" href="/">Return to the homepage</a></p>
+	</div>
+	    <br style="clear: both;" />
+</div>

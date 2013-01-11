ea11563938f01c3b7ddf23c92b91c0e96b3c69bd
flexage <alex@penoval.co.uk>
2013-01-11 11:58:18 +0000

    Added a few small fixes to Issue #42: Implement Custom 404 - File Not Found Error Pages
    Details: Fixed typo on "page cannot be found" message, changed "return to homepage" href to use the $baseurl ability. Added a "Question Mark Mario Block" icon while I was at it.

--- tab_404.php
+++ tab_404.php
@@ -1,10 +1,10 @@
 <div id="gameHead">
     <img src="/images/404.jpg" style="float:left;padding: 20px; padding-right: 50px;" />
     <div style="text-align: left; margin-bottom: 20px; ">
-	    <h1 style="margin-top: 80px">404 - Page not found</h1>
-	    <p>The page you are looking for cannot be found.<br/>Please check your URL or notify us when this page is suppost to exist.</p>
+	    <h1 style="margin-top: 80px"><img src="<?= $baseurl ?>/images/common/icons/question-block_48.png" alt="404 Icon" style="vertical-align: middle; margin-right: 20px;" />404 - Page not found</h1>
+	    <p>The page you are looking for cannot be found.<br/>Please check your URL or notify us if this page is supposed to exist.</p>
 	    <br/>
-	    <p><a style="color: orange" href="/">Return to the homepage</a></p>
+	    <p><a style="color: orange" href="<?= $baseurl ?>">Return to the homepage</a></p>
 	</div>
 	    <br style="clear: both;" />
 </div>

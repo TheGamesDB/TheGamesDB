fc9607afc5b9affb12d3d92e4a0910018bf6f628
macdguy <macdguy@gmail.com>
2014-03-25 21:40:45 -0700

    Fixed bug where you can comment with a blank comment.

--- modules/mod_comment.php
+++ modules/mod_comment.php
@@ -1,33 +1,39 @@
 <?php
-
 	/*
 	 * Comments Functions
 	 */
 
 	function check_input($value)
 	{
-	// Stripslashes
-	if (get_magic_quotes_gpc())
-	  {
-	  $value = stripslashes($value);
-	  }
-	// Quote if not a number
-	if (!is_numeric($value))
-	  {
-	  $value = "'" . mysql_real_escape_string($value) . "'";
-	  }
-	return $value;
+		// Stripslashes
+		if (get_magic_quotes_gpc())
+		{
+			$value = stripslashes($value);
+		}
+
+		// Quote if not a number
+		if (!is_numeric($value))
+		{
+			$value = "'" . mysql_real_escape_string($value) . "'";
+		}
+		return $value;
 	}
 	 
-	if ($function == 'Add Game Comment') {
+	if ($function == 'Add Game Comment')
+	{
 		$comment = htmlspecialchars($comment, ENT_QUOTES);
 		$userid = check_input($userid);
 		$gameid = check_input($gameid);
-		$commentQuery = mysql_query(" INSERT INTO comments (userid, gameid, comment, timestamp) VALUES ('$userid', '$gameid', '$comment', FROM_UNIXTIME($time)) ") or die('Query failed: ' . mysql_error());
+
+		if (strlen($comment)>1)
+		{
+			mysql_query("INSERT INTO comments (userid, gameid, comment, timestamp) VALUES ('$userid', '$gameid', '$comment', FROM_UNIXTIME($time))") or die('Query failed: ' . mysql_error());
+		}
 	}
 
-	if ($function == 'Delete Game Comment') {
-		$commentQuery = mysql_query(" DELETE FROM comments WHERE id = $commentid ") or die('Query failed: ' . mysql_error());
+	if ($function == 'Delete Game Comment')
+	{
+		mysql_query("DELETE FROM comments WHERE id = $commentid") or die('Query failed: ' . mysql_error());
 	}
 
 ?>

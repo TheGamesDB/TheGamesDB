2a3d4f163cb1e8eda35b4580e757b665fdd5e138
macdguy <macdguy@gmail.com>
2014-03-29 21:01:03 -0700

    Fixed conflicts from last merge.

--- modules/mod_comment.php
+++ modules/mod_comment.php
@@ -3,27 +3,32 @@
 	/*
 	 * Comments Functions
 	 */
-
 	function check_input($value)
 	{
-	// Stripslashes
-	if (get_magic_quotes_gpc())
-	  {
-	  $value = stripslashes($value);
-	  }
-	// Quote if not a number
-	if (!is_numeric($value))
-	  {
-	  $value = "'" . mysql_real_escape_string($value) . "'";
-	  }
-	return $value;
+		// Stripslashes
+		if (get_magic_quotes_gpc())
+		{
+			$value = stripslashes($value);
+		}
+		
+		// Quote if not a number
+		if (!is_numeric($value))
+		{
+			$value = "'" . mysql_real_escape_string($value) . "'";
+		}
+
+		return $value;
 	}
-	 
+
 	if (isset($function) && $function == 'Add Game Comment') {
 		$comment = htmlspecialchars($comment, ENT_QUOTES);
 		$userid = check_input($userid);
 		$gameid = check_input($gameid);
-		$commentQuery = mysql_query(" INSERT INTO comments (userid, gameid, comment, timestamp) VALUES ('$userid', '$gameid', '$comment', FROM_UNIXTIME($time)) ") or die('Query failed: ' . mysql_error());
+
+		if (strlen($comment)>1)
+		{
+			$commentQuery = mysql_query(" INSERT INTO comments (userid, gameid, comment, timestamp) VALUES ('$userid', '$gameid', '$comment', FROM_UNIXTIME($time)) ") or die('Query failed: ' . mysql_error());
+		}
 	}
 
 	if (isset($function) && $function == 'Delete Game Comment') {

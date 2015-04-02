def2ac4604feeeddcbd15971e7c8c57753e3de57
Leandro Lisboa Penz <lpenz@lpenz.org>
2015-04-01 23:59:06 -0300

    Provide a VM for tests, using vagrant and ansible.
    This commit adds a Vagrantfile that brings up the standard ubuntu/trusty64
    vagrant virtualbox VM, provisions it with the required apache, PHP and
    MySQL packages and configurations, and installs the site. To do that,
    install vagrant on the host and do a "vagrant up" in the working copy
    of the repository. The site will be available at http://localhost:8888/

---
+++ ansible/config.php
@@ -0,0 +1,24 @@
+<?php
+	## Misc Settings
+	$baseurl = "";
+	$apache_type = "unix";  // Types are unix or windows
+
+	## DB Settings
+	$db_user = "root"; // Database Username
+	$db_password = ""; // Database Password
+	$db_database = "thegamedb"; // Database Name
+	$db_server = "localhost"; // Usually Localhost
+
+	## Mail Settings
+	$mail_server = "localhost";
+	$mail_username = "";
+	$mail_password = "";
+
+	## Recaptcha Settings
+	// (Developers will need to get their own keys from recaptcha, if you are running from 'localhost' try making the key usable for 'all domains')
+	$recpatcha_publickey = "";
+    $recaptcha_privatekey = "";
+
+    ## Timezone Settings
+	date_default_timezone_set('UTC'); 
+?>
